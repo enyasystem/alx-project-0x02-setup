82727e0acc59ca1ef8a82150eebe9bb2d4d834e0
enyasystem <enyaelvis@gmail.com>
2025-11-23 22:47:36 +0100

    refactor: clean up Button component by removing redundant type definition

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,11 +1,8 @@
-type ButtonProps = {
-  size?: 'small' | 'medium' | 'large'
-  shape?: 'rounded-sm' | 'rounded-md' | 'rounded-full'
-  children: React.ReactNode
-}
+import { type ButtonProps } from '@/interfaces'
 
-export default function Button({ size='medium', shape='rounded-md', children }: ButtonProps) {
+export default function Button({ size = 'medium', shape = 'rounded-md', children }: ButtonProps) {
   const sizeClass = size === 'small' ? 'px-2 py-1 text-sm' : size === 'large' ? 'px-6 py-3 text-lg' : 'px-4 py-2'
+
   return (
     <button className={`${sizeClass} ${shape} bg-gray-800 text-white`}>{children}</button>
   )
